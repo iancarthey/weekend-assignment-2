3322cb72d64751254aaac2c15b6ed0547b4570c5
Ian Carthey <PimpDaddyE@Ians-MacBook-Pro-2.local>
2018-03-16 18:32:56 -0500

    completed base mode

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,110 @@ $(document).ready(readyNow);
 // linking jQuery
 function readyNow(){
     console.log('jQuery linked');
+    $('#additionButton').on('click', addNumbers);
+    $('#subtractButton').on('click', subtractNumbers);
+    $('#multiplyButton').on('click', multiplyNumbers);
+    $('#divisionButton').on('click', divideNumbers);
+    $('#clearButton').on('click', refreshPage);
+    getProblem();
+}
+
+//creating class for numbers
+class Value{
+    constructor( number1In, number2In ){
+        this.number1 = number1In;
+        this.number2 = number2In;
+        this.operator = " ";        
+        this.answer = " ";
+    }
+}
+
+// using to create a new class every round
+function sendProblem(newNumbers){
+    $.ajax({
+        type: "POST",
+        data: newNumbers,
+        url: '/calc'
+    }).done(function(response){
+        getProblem();
+        console.log(response);
+    }).fail(function(response){
+        alert('something went wrong');
+    })
+}
+
+//function get request
+function getProblem(){
+    $.ajax({
+        type: "GET",
+        url: "/calc"
+    }).done(function(response){
+        console.log(response);
+        appendToDom(response);
+    })
+}
+
+//function for addition click listener
+function addNumbers(){
+    let number1 = $('#input1').val();
+    let number2 = $('#input2').val();
+    let newNumbers = new Value(number1, number2);
+    newNumbers.operator = 'add';
+    console.log(newNumbers);
+    sendProblem(newNumbers);
+}
+
+//function for subtraction click listener
+function subtractNumbers(){
+    let number1 = $('#input1').val();
+    let number2 = $('#input2').val();
+    let newNumbers = new Value(number1, number2);
+    newNumbers.operator = 'subtract';
+    console.log(newNumbers);
+    sendProblem(newNumbers);
+}
+
+//function for multiply click listener
+function multiplyNumbers(){
+    let number1 = $('#input1').val();
+    let number2 = $('#input2').val();
+    let newNumbers = new Value(number1, number2);
+    newNumbers.operator = 'multiply';
+    console.log(newNumbers);
+    sendProblem(newNumbers);
+}
+
+//function for division click listener
+function divideNumbers(){
+    let number1 = $('#input1').val();
+    let number2 = $('#input2').val();
+    let newNumbers = new Value(number1, number2);
+    newNumbers.operator = 'divide';
+    console.log(newNumbers);
+    sendProblem(newNumbers);
+}
+
+//appending to dom
+function appendToDom(response){
+    $('#calculatorBody').empty();
+    for(let newNumbers of response){
+        console.log(newNumbers);
+        tr = $('<tr></tr>');
+        tr.append('<td>' + newNumbers.number1 + '</td>');
+        tr.append('<td>' + newNumbers.number2 + '</td>');
+        tr.append('<td>' + newNumbers.answer + '</td>');
+        $('#calculatorBody').append(tr);
+    }
+}
+
+//functiont to clear
+function refreshPage(){
+    $.ajax({
+        type: "DELETE",
+        url: "/calc"
+    }).done(function(response){
+        console.log(response);
+        appendToDom(response);
+        
+    })
 }
--- server/server.js
+++ server/server.js
@@ -1,3 +1,43 @@
 let express = require('express');
 let app = express();
 const PORT = 5000;
+let bodyParser = require('body-parser');
+
+let problemArray = [];
+
+//adding body-parser
+app.use(bodyParser.urlencoded({extended:true}));
+
+//server static files
+app.use(express.static('server/public'));
+
+app.post('/calc', (req,res) => {
+    console.log(req.body);
+    let newNumbers = req.body;
+    if( newNumbers.operator === 'add'){
+        newNumbers.answer = parseInt(newNumbers.number1) + parseInt(newNumbers.number2);
+    } else if( newNumbers.operator === 'subtract' ){
+        newNumbers.answer = parseInt(newNumbers.number1) - parseInt(newNumbers.number2);
+    } else if( newNumbers.operator === 'multiply' ){
+        newNumbers.answer = parseInt(newNumbers.number1) * parseInt(newNumbers.number2);
+    } else if( newNumbers.operator === 'divide' ){
+        newNumbers.answer = parseInt(newNumbers.number1) / parseInt(newNumbers.number2);
+    };
+    problemArray.push(newNumbers);
+    console.log(problemArray);
+    res.sendStatus(200);
+})
+
+app.get('/calc', (req,res) =>{
+    res.send(problemArray);
+})
+
+// starting up the server
+app.listen(PORT, () => {
+    console.log('server is running on port: ', PORT);
+})
+
+app.delete('/calc', (req,res) =>{
+    problemArray = [];
+    res.send(problemArray);
+})
