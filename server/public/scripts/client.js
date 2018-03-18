13c81ba63df029fb5de4961ee54bd3190b9253cd
Ian Carthey <PimpDaddyE@Ians-MacBook-Pro-2.local>
2018-03-18 16:06:18 -0500

    added number buttons and number click listeners

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,13 +4,26 @@ $(document).ready(readyNow);
 // linking jQuery
 function readyNow(){
     console.log('jQuery linked');
+    $('#oneButton').on('click', oneFunk);
+    $('#twoButton').on('click', twoFunk);
+    $('#threeButton').on('click', threeFunk);
+    $('#fourButton').on('click', fourFunk);
+    $('#fiveButton').on('click', fiveFunk);
+    $('#sixButton').on('click', sixFunk);
+    $('#sevenButton').on('click', sevenFunk);
+    $('#eightButton').on('click', eightFunk);
+    $('#nineButton').on('click', nineFunk);
+    $('#zeroButton').on('click', zeroFunk);
     $('#additionButton').on('click', addNumbers);
     $('#subtractButton').on('click', subtractNumbers);
     $('#multiplyButton').on('click', multiplyNumbers);
     $('#divisionButton').on('click', divideNumbers);
+    $('#equalsButton').on('click', equalNumbers);
     $('#clearButton').on('click', refreshPage);
     getProblem();
 }
+
+let numbersArray = [];
 
 //creating class for numbers
 class Value{
@@ -54,7 +67,7 @@ function addNumbers(){
     let newNumbers = new Value(number1, number2);
     newNumbers.operator = '+';
     console.log(newNumbers);
-    sendProblem(newNumbers);
+    // sendProblem(newNumbers);
 }
 
 //function for subtraction click listener
@@ -64,7 +77,7 @@ function subtractNumbers(){
     let newNumbers = new Value(number1, number2);
     newNumbers.operator = '-';
     console.log(newNumbers);
-    sendProblem(newNumbers);
+    // sendProblem(newNumbers);
 }
 
 //function for multiply click listener
@@ -74,7 +87,7 @@ function multiplyNumbers(){
     let newNumbers = new Value(number1, number2);
     newNumbers.operator = 'x';
     console.log(newNumbers);
-    sendProblem(newNumbers);
+    // sendProblem(newNumbers);
 }
 
 //function for division click listener
@@ -84,7 +97,7 @@ function divideNumbers(){
     let newNumbers = new Value(number1, number2);
     newNumbers.operator = '/';
     console.log(newNumbers);
-    sendProblem(newNumbers);
+    // sendProblem(newNumbers);
 }
 
 //appending to dom
@@ -100,12 +113,10 @@ function appendToDom(response){
         tr.append('<td>' + newNumbers.number2 + ' =</td>');
         tr.append('<td>' + newNumbers.answer + '</td>');
         $('#calculatorBody').append(tr);
-        // $('#answerHead').empty()
-        // $('#answerHead').append('<h2>The Answer is: ' + newNumbers.answer + '</h2>');
     }
 }
 
-//functiont to clear
+//function to clear
 function refreshPage(){
     $.ajax({
         type: "DELETE",
@@ -115,4 +126,79 @@ function refreshPage(){
         appendToDom(response);
         
     })
+}
+
+//function for equals click listener
+function equalNumbers(){
+    sendProblem(newNumbers);
+}
+
+//function for clicking the one button
+function oneFunk(){
+    let inputValue = $('#input1').val(1);
+    numbersArray.push(1);
+    console.log(numbersArray);
+}
+
+//function for clicking the two button
+function twoFunk(){
+    let inputValue = $('#input1').val(2);
+    numbersArray.push(2);
+    console.log(numbersArray);
+}
+
+//function for clicking the three button
+function threeFunk(){
+    let inputValue = $('#input1').val(3);
+    numbersArray.push(3);
+    console.log(numbersArray);
+}
+
+//function for clicking the four button
+function fourFunk(){
+    let inputValue = $('#input1').val(4);
+    numbersArray.push(4);
+    console.log(numbersArray);
+}
+
+//function for clicking the five button
+function fiveFunk(){
+    let inputValue = $('#input1').val(5);
+    numbersArray.push(5);
+    console.log(numbersArray);
+}
+
+//function for clicking the six button
+function sixFunk(){
+    let inputValue = $('#input1').val(6);
+    numbersArray.push(6);
+    console.log(numbersArray);
+}
+
+//function for clicking the seven button
+function sevenFunk(){
+    let inputValue = $('#input1').val(7);
+    numbersArray.push(7);
+    console.log(numbersArray);
+}
+
+//function for clicking the eight button
+function eightFunk(){
+    let inputValue = $('#input1').val(8);
+    numbersArray.push(8);
+    console.log(numbersArray);
+}
+
+//function for clicking the nine button
+function nineFunk(){
+    let inputValue = $('#input1').val(9);
+    numbersArray.push(9);
+    console.log(numbersArray);
+}
+
+//function for clicking the zero button
+function zeroFunk(){
+    let inputValue = $('#input1').val(0);
+    numbersArray.push(0);
+    console.log(numbersArray);
 }
